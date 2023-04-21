bda0513d3d95da0da67cd37ec31e92cb606cafd0
Avanish <79317546+avanishbgit@users.noreply.github.com>
2023-04-21 12:06:13 +0530

    Added App with some placeholders

--- src/main/java/org/lava/trender/App.java
+++ src/main/java/org/lava/trender/App.java
@@ -1,13 +1,20 @@
 package org.lava.trender;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javax.swing.JFrame;
+
+public class App {
+    public static void main( String[] args ) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Lava Project");
+        frame.setResizable(false);
+        App game = new App();
+        frame.add(game);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setAlwaysOnTop(false);
+        frame.setVisible(true);
+
+        game.start();
     }
 }
